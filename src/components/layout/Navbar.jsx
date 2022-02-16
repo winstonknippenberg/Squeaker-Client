221bf24a02f333adbe08723be923e46368e55c4e
= <=>
2022-02-16 11:16:35 +0200

    NavBar logo adittion

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -4,12 +4,13 @@ import { PropTypes } from 'prop-types';
 import MyButton from '../../utils/MyButton';
 import PostSqueak from '../squeaks/PostSqueak';
 import Notifications from './Notifications';
+import Logo from '../../images/hamsterLogo.png';
 //mui
 import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import HomeIcon from '@mui/icons-material/Home';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
 //redux
 import { connect } from 'react-redux';
 
@@ -18,31 +19,42 @@ class Navbar extends Component {
     const { authenticated } = this.props;
     return (
       <AppBar>
-        <Toolbar className="nav-container">
-          {authenticated ? (
-            <Fragment>
-              <PostSqueak />
-              <Link to="/">
-                <MyButton tip="Home">
-                  <HomeIcon color="primary.contrastText" />
-                </MyButton>
-              </Link>
-              <Notifications />
-            </Fragment>
-          ) : (
-            <Fragment>
-              <Button color="inherit" component={Link} to="/login">
-                <Typography variant="body2">Login</Typography>
-              </Button>
-              <Button color="inherit" component={Link} to="/">
-                <Typography variant="body2">Home</Typography>
-              </Button>
-              <Button color="inherit" component={Link} to="/signup">
-                <Typography variant="body2">Signup</Typography>
-              </Button>
-            </Fragment>
-          )}
-        </Toolbar>
+        <Grid container className="navGrid">
+          <Grid item sm className="navLogo">
+            <div>
+              <img src={Logo} alt="Logo" className="logo" />
+            </div>
+          </Grid>
+          <Grid item sm className="navGridItem">
+            {authenticated ? (
+              <Fragment>
+                <PostSqueak />
+                <Link to="/">
+                  <MyButton tip="Home">
+                    <HomeIcon color="primary.contrastText" />
+                  </MyButton>
+                </Link>
+                <Notifications />
+              </Fragment>
+            ) : (
+              <Fragment>
+                <Button color="inherit" component={Link} to="/login">
+                  <Typography variant="body2">Login</Typography>
+                </Button>
+                <Button color="inherit" component={Link} to="/">
+                  <Typography variant="body2">Home</Typography>
+                </Button>
+                <Button color="inherit" component={Link} to="/signup">
+                  <Typography variant="body2">Signup</Typography>
+                </Button>
+              </Fragment>
+            )}
+          </Grid>
+          <Grid item sm />
+        </Grid>
+
+        <div className="routes"></div>
+        <div className="blankNavItem"></div>
       </AppBar>
     );
   }
